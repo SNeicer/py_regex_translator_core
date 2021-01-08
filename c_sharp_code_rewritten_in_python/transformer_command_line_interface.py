fd818a1266ef0adaafe5717b4690891dafdd0a20
SNeicer <unconfigured@null.spigotmc.org>
2021-01-08 19:01:26 +0500

    Update transformer_command_line_interface.py

---
+++ c_sharp_code_rewritten_in_python/transformer_command_line_interface.py
@@ -0,0 +1,11 @@
+from c_sharp_code_rewritten_in_python import interfaces
+
+class TransformerCLI:
+
+    def __init__(self, transformer: interfaces.IFileTransformer):
+        self._transformer = transformer
+
+    def run(self, args = []):
+        sourcePath = args[0]
+        targetPath = args[0]
+        self._transformer.transform(sourcePath, targetPath)
